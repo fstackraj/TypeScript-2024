1b844bfeefc5406f56833c53fab03e342a5dc5df
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-06 12:49:29 +0530

    Learning ts basics 2024

---
+++ 01_basic.ts
@@ -0,0 +1,12 @@
+export {};
+
+let name = "TypeScript";
+// name = 5656;
+
+let age = 20;
+// age = true;
+
+let typeBoolean: boolean;
+typeBoolean = true;
+typeBoolean = false;
+// typeBoolean = 1;
---
+++ 02_union.ts
@@ -0,0 +1,8 @@
+export {};
+
+//  union type in typescript
+
+let unionType: number | string;
+unionType = 10;
+unionType = "Hello";
+// unionType = true; // error
